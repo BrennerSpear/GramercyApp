df643481045c754454b950a9af1bfb03431935f1
BrennerSpear <Blspear@gmail.com>
2015-07-29 09:30:45 -0700

    added header & Brand

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :brands
+  root 'pages#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
