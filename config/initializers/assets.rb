81b2820a485939d5a4366fb7eb5acbfeec12281d
BrennerSpear <Blspear@gmail.com>
2015-08-01 20:11:10 -0700

    added benefits page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,7 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( custom_home.js )
+Rails.application.config.assets.precompile += %w( pages.css )
+Rails.application.config.assets.precompile += %w( home.css )
+Rails.application.config.assets.precompile += %w( payment_model.css )
+Rails.application.config.assets.precompile += %w( benefits.css )
