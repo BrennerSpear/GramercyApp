57eff4672c535bfe7effb3eccfbce94e0c9f6184
BrennerSpear <Blspear@gmail.com>
2015-08-03 12:03:21 -0700

    fixed syntax err

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = {host: 'https://gramercy-app.herokuapp.com/'}
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smto_settings = {
+  config.action_mailer.smtp_settings = {
     address:        'smtp.sendgrid.net',
     port:           '587',
     authentication: :plain,
