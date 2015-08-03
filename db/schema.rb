9eafd1af17e6d1712a5288d2f77e17645fc342f3
BrennerSpear <Blspear@gmail.com>
2015-08-02 22:43:55 -0700

    raked db

--- db/schema.rb
+++ db/schema.rb
@@ -13,27 +13,30 @@
 
 ActiveRecord::Schema.define(version: 20150730212838) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "brands", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.text     "email",                  default: "", null: false
+    t.text     "encrypted_password",     default: "", null: false
+    t.text     "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.text     "current_sign_in_ip"
+    t.text     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
   end
 
-  add_index "brands", ["email"], name: "index_brands_on_email", unique: true
-  add_index "brands", ["reset_password_token"], name: "index_brands_on_reset_password_token", unique: true
+  add_index "brands", ["email"], name: "index_brands_on_email", unique: true, using: :btree
+  add_index "brands", ["reset_password_token"], name: "index_brands_on_reset_password_token", unique: true, using: :btree
 
   create_table "leads", force: :cascade do |t|
-    t.string   "email"
-    t.string   "platform"
+    t.text     "email"
+    t.text     "platform"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
