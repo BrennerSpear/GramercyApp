041c7f5b5834863b1e2c47123eeb615abdaabced
BrennerSpear <Blspear@gmail.com>
2015-08-06 12:13:44 -0700

    g shopify app. root to: home#index temporarily

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
+  
+  controller :sessions do
+    get 'login' => :new, :as => :login
+    post 'login' => :create, :as => :authenticate
+    get 'auth/shopify/callback' => :callback
+    get 'logout' => :destroy, :as => :logout
+  end
+
+  root :to => 'home#index'
+
+  
   devise_for :admins
   resources :leads
 
   devise_for :brands
-
-  root 'pages#home'
 
 
   get 'benefits' => 'pages#benefits'
