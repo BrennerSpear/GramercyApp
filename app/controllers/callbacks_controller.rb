a5668865fbff856293a734b3da6e81c2ea621cc4
BrennerSpear <Blspear@gmail.com>
2015-08-12 21:13:02 -0700

    routing correctly

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,16 +1,13 @@
 class CallbacksController < ApplicationController
 
 	def post
-
-		render text: "TESTING"
-		# case request.method
-		# when "GET"
-		# 	@challenge = params['hub.challenge']
-		# 	render text: @challenge
-		# when "POST"
-		# 	flash[:notice] = "something with instagram worked"
-		# 	render nothing: true
-		# end
+		case request.method
+		when "GET"		
+			render text: params['hub.challenge']
+		when "POST"
+			flash[:notice] = "something with instagram worked"
+			render nothing: true
+		end
 	end
 
 end
