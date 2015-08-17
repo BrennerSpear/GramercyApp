02fb2557f5a3895044e8ef67ade0f28384319eaf
BrennerSpear <Blspear@gmail.com>
2015-08-17 14:02:58 -0700

    fixed id-uid issue

--- app/workers/update_followers_worker.rb
+++ app/workers/update_followers_worker.rb
@@ -4,9 +4,9 @@ class UpdateFollowersWorker
 
 	def perform(new_post)
 
-		shopper_id = new_post["object_id"]
+		shopper_uid = new_post["object_id"]
 
-		@shopper = Shopper.find(shopper_id)
+		@shopper = Shopper.find_by_uid(shopper_uid)
 		Shopper.update_followers(@shopper, type)
 	end
 end
